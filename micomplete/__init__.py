848c4b5bf265e5660d8e63285cc22dc9258b138b
EricHugo <eric@hugoson.org>
2017-07-29 23:59:24 +0200

    added package init

--- micomplete/__init__.py
+++ micomplete/__init__.py
@@ -1,3 +1,6 @@
+# Copyright (c) Eric Hugoson.
+# See LICENSE for details.
+
 from .completeness import calcCompleteness
 from .linkageanalysis import linkageAnalysis
 from .parseseqs import parseSeqStats 
