719a99e68841604ae826c5113de180fa53ce7cb6
Eric Hugoson <eric@hugoson.org>
2017-09-04 16:52:12 +0200

    added __all__ function

--- micomplete/__init__.py
+++ micomplete/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) Eric Hugoson.
 # See LICENSE for details.
 
+__all__ = ['completeness', 'linkageanalysis', 'parseseqs']
 from .completeness import calcCompleteness
 from .linkageanalysis import linkageAnalysis
 from .parseseqs import parseSeqStats 
