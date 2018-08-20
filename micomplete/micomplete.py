61743c0b6a87f25ba8381d53f553b702c44a6be4
eric.hugoson <hugoson@evolbio.mpg.de>
2018-08-20 15:58:15 +0200

    linted

--- micomplete/micomplete.py
+++ micomplete/micomplete.py
@@ -6,24 +6,23 @@ Copyright and License
 Copyright 2017 Eric Hugoson and Lionel Guy
 
 This program is free software: you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free Software 
-Foundation, either version 3 of the License, or (at your option) any later 
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
 version.
 
 This program is distributed in the hope that it will be useful, but WITHOUT ANY
 WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-PARTICULAR PURPOSE. 
+PARTICULAR PURPOSE.
 
 See the GNU General Public License for more details.
-You should have received a copy of the GNU General Public License along with 
+You should have received a copy of the GNU General Public License along with
 this program.  If not, see http://www.gnu.org/licenses/.
 """
 
 from __future__ import print_function, division
 from distutils import spawn
-from collections import defaultdict, namedtuple
+from collections import defaultdict
 from Bio import SeqIO
-from Bio.Seq import Seq
 from Bio.SeqUtils import GC
 from operator import itemgetter
 from itertools import chain
@@ -60,100 +59,102 @@ except ImportError:
     from linkageanalysis import linkageAnalysis
     from completeness import calcCompleteness
 
-def _worker(seqObject, seqType, argv, q=None, name=None):
+def _worker(seqObject, seq_type, argv, q=None, name=None):
     seqObject = ''.join(seqObject)
-    baseName = os.path.basename(seqObject).split('.')[0]
+    base_name = os.path.basename(seqObject).split('.')[0]
     if not name:
-        name = baseName
+        name = base_name
     if argv.linkage or argv.completeness:
-        if re.match("(gb.?.?)|genbank", seqType):
+        if re.match("(gb.?.?)|genbank", seq_type):
             proteome = extract_gbk_trans(seqObject)
             if os.stat(proteome).st_size == 0:
                 contigs = get_contigs_gbk(seqObject, name=name)
                 proteome = create_proteome(contigs, name)
-        elif seqType == "fna":
+        elif seq_type == "fna":
             proteome = create_proteome(seqObject, name)
-        elif seqType == "faa":
+        elif seq_type == "faa":
             proteome = seqObject
     else:
         proteome = False
-    fastats = parseSeqStats(seqObject, name, seqType)
-    seqLength, allLengths, GCcontent = fastats.get_length()
-    seqstats = (fastats, seqLength, allLengths, GCcontent)
+    fastats = parseSeqStats(seqObject, name, seq_type)
+    seq_length, all_lengths, GCcontent = fastats.get_length()
+    seqstats = (fastats, seq_length, all_lengths, GCcontent)
     if argv.linkage:
         try:
             assert argv.hmms
         except (AssertionError, NameError):
             raise NameError("A set of HMMs must be provided to calculate linkage")
-        comp = calcCompleteness(proteome, name, argv.hmms, evalue=argv.evalue, 
-                weights=argv.weights, hlist=argv.hlist, linkage=argv.linkage, 
-                lenient=argv.lenient, debug=argv.debug)
-        hmmMatches, dupHmms, totalHmms = comp.get_completeness()
+        comp = calcCompleteness(proteome, name, argv.hmms, evalue=argv.evalue,
+                                weights=argv.weights, hlist=argv.hlist,
+                                linkage=argv.linkage, lenient=argv.lenient,
+                                debug=argv.debug)
+        hmm_matches, _, total_hmms = comp.get_completeness()
         if argv.hlist:
             comp.print_hmm_lists(directory=argv.hlist)
         try:
-            fracHmm = len(hmmMatches) / len(totalHmms)
+            frac_hmm = len(hmm_matches) / len(total_hmms)
         except TypeError:
-            fracHmm = 0
-        if fracHmm < argv.cutoff:
-            percHmm = fracHmm * 100
+            frac_hmm = 0
+        if frac_hmm < argv.cutoff:
+            perc_hmm = frac_hmm * 100
             cprint("Warning:", "red", end=' ', file=sys.stderr)
-            print("%i%% of markers were found in %s, cannot be used to calculate linkage" 
-        % (percHmm, name), file=sys.stderr)
+            print("%i%% of markers were found in %s, cannot be used to calculate\
+                   linkage" % (perc_hmm, name), file=sys.stderr)
             return
-        linkage = linkageAnalysis(seqObject, name, seqType, 
-                proteome, seqstats, hmmMatches, argv.debug, q)
-        linkageVals = linkage.calculate_linkage_scores()
-        for hmm, match in hmmMatches.items():
-            linkageVals[hmm].append(match)
-        q.put(linkageVals)
-        return linkageVals
-    compile_results(seqType, name, argv, proteome, seqstats, q)
+        linkage = linkageAnalysis(seqObject, name, seq_type, proteome, seqstats,
+                                  hmm_matches, argv.debug, q)
+        linkage_vals = linkage.calculate_linkage_scores()
+        for hmm, match in hmm_matches.items():
+            linkage_vals[hmm].append(match)
+        q.put(linkage_vals)
+        return linkage_vals
+    compile_results(seq_type, name, argv, proteome, seqstats, q)
 
-def compile_results(seqType, name, argv, proteome, seqstats, q=None):
+def compile_results(seq_type, name, argv, proteome, seqstats, q=None):
     """
     Compile results from sequences passed to requested modules and pass
     to Queue for thread safe output. If no Queue given print directly to
     stdout.
     """
     output = []
-    if not seqType == 'faa':
-        fastats, seqLength, allLengths, GC = seqstats
+    if not seq_type == 'faa':
+        fastats, seq_length, all_lengths, GC = seqstats
     else:
-        fastats, seqLength, allLengths, GC = "-", "-", "-", "-"
-    output.extend((name, seqLength, GC))
+        fastats, seq_length, all_lengths, GC = "-", "-", "-", "-"
+    output.extend((name, seq_length, GC))
     if argv.completeness:
-        comp = calcCompleteness(proteome, name, argv.hmms, evalue=argv.evalue, 
-                weights=argv.weights, hlist=argv.hlist, linkage=argv.linkage, 
-                lenient=argv.lenient, debug=argv.debug)
-        filledHmms, redunHmms, totalHmms = comp.quantify_completeness()
+        comp = calcCompleteness(proteome, name, argv.hmms, evalue=argv.evalue,
+                                weights=argv.weights, hlist=argv.hlist,
+                                linkage=argv.linkage, lenient=argv.lenient,
+                                debug=argv.debug)
+        filled_hmms, redun_hmms, total_hmms = comp.quantify_completeness()
         try:
-            numHmms = filledHmms
+            num_hmms = filled_hmms
         except TypeError:
-            numHmms = 0
-        output.append(numHmms)
+            num_hmms = 0
+        output.append(num_hmms)
         try:
-            markerComp = '%0.3f' % (round(numHmms / totalHmms, 3))
+            marker_comp = '%0.3f' % (round(num_hmms / total_hmms, 3))
         except ZeroDivisionError:
-            markerComp = 0
-        output.append(markerComp)
+            marker_comp = 0
+        output.append(marker_comp)
         try:
-            redundance = '%0.3f' % (round((redunHmms) / numHmms, 3))
+            redundance = '%0.3f' % (round((redun_hmms) / num_hmms, 3))
         except ZeroDivisionError:
             redundance = 0
         output.append(redundance)
         if argv.weights:
-            if numHmms > 0:
-                weightedComp, weightedRedun = comp.attribute_weights(numHmms)
+            if num_hmms > 0:
+                weighted_comp, weighted_redun = comp.attribute_weights(num_hmms)
             else:
-                weightedComp, weightedRedun = 0, 0
-            output.append('%0.3f' % weightedComp)
-            output.append('%0.3f' % weightedRedun)
+                weighted_comp, weighted_redun = 0, 0
+            output.append('%0.3f' % weighted_comp)
+            output.append('%0.3f' % weighted_redun)
     # only calculate assembly stats if filetype is fna
     if argv.hlist:
         comp.print_hmm_lists(directory=argv.hlist)
-    if not re.match("(gb.?.?)|genbank|faa", seqType): 
-        N50, L50, N90, L90 = fastats.get_stats(seqLength, allLengths)
+    if not re.match("(gb.?.?)|genbank|faa", seq_type):
+        N50, L50, N90, L90 = fastats.get_stats(seq_length, all_lengths)
     else:
         N50, L50, N90, L90 = '-', '-', '-', '-'
     output.extend((N50, L50, N90, L90))
@@ -170,7 +171,7 @@ def _listener(q):
     Function responsible for outputting information in a thread safe manner.
     Recieves write requests from Queue and writes different targets depending
     on type. Writes results from general operation to stdout, logobjects
-    to logfile and any calculated weights of each organism to unified 
+    to logfile and any calculated weights of each organism to unified
     tmp file.
     """
     weights_file = "micomplete_weights.temp"
@@ -181,20 +182,20 @@ def _listener(q):
         #print(write_request)
         if write_request == 'done':
             break
-        if type(write_request) is str:
+        if isinstance(write_request, str):
             #logger.handle(write_request)
             continue
-        if type(write_request) is list:
+        if isinstance(write_request, list):
             if sys.version_info > (3, 0):
                 print(*write_request, sep='\t')
             else:
                 print('\t'.join(map(str, write_request)))
             continue
-        for hmm, match in sorted(write_request.items(), 
-                key=lambda e: e[1], reverse=True):
-            { all_bias[hmm].append(1) if float(stats[3]) / float(stats[2]) > 0.1 
-                    else all_bias[hmm].append(0) for 
-                    stats in match[1] }
+        for hmm, match in sorted(write_request.items(), key=lambda e: e[1],
+                                 reverse=True):
+            {all_bias[hmm].append(1) if float(stats[3]) / float(stats[2]) > 0.1
+             else all_bias[hmm].append(0) for
+             stats in match[1]}
             weight = (str(hmm) + '\t' + str(match[0]) + '\n')
             weights_tmp.write(weight)
         weights_tmp.write('-\n')
@@ -202,12 +203,11 @@ def _listener(q):
     for hmm, bias in all_bias.items():
         total_fraction_bias = sum(bias) / len(bias)
         if total_fraction_bias > 0.5:
-            cprint("Warning:", "red", file=sys.stderr, end=' ') 
-            print("More than %s%% of found markers had a higher than %s%% of" % 
-                    (0.5 * 100, 0.1 * 100), file=sys.stderr,
-                    end=' ')
-            print("score bias in marker %s. Consider not using this marker." % 
-                    hmm, file=sys.stderr)
+            cprint("Warning:", "red", file=sys.stderr, end=' ')
+            print("More than %s%% of found markers had a higher than %s%% of" %
+                  (0.5 * 100, 0.1 * 100), file=sys.stderr, end=' ')
+            print("score bias in marker %s. Consider not using this marker." %
+                  hmm, file=sys.stderr)
     weights_tmp.close()
     return weights_file
 
@@ -242,12 +242,12 @@ def weights_output(weights_file):
     plt.boxplot(data, vert=False, sym='+')
     ax.set_yticklabels(labels, fontsize=8)
     ax.xaxis.grid(True, linestyle='-', which='major', color='lightgrey',
-            alpha=0.5)
+                  alpha=0.5)
     ax.set_xlabel('Relative linkage distance')
     ax.set_ylabel('Markers')
     plt.show()
 
-def create_proteome(fasta, baseName=None):
+def create_proteome(fasta, base_name=None):
     """Create proteome from given .fna file, returns proteome filename"""
     try:
         assert shutil.which('prodigal')
@@ -258,16 +258,16 @@ def create_proteome(fasta, baseName=None):
             assert spawn.find_executable('prodigal')
         except AssertionError:
             raise RuntimeError('Unable to find prodigal in path')
-    if not baseName:
-        baseName = os.path.basename(fasta).split('.')[0]
-    protFileName = baseName + "_prodigal.faa"
+    if not base_name:
+        base_name = os.path.basename(fasta).split('.')[0]
+    prot_filename = base_name + "_prodigal.faa"
     if sys.version_info > (3, 4):
-        subprocess.run(['prodigal', '-i', fasta, '-a', protFileName], 
-                stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        subprocess.run(['prodigal', '-i', fasta, '-a', prot_filename],
+                       stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     else:
-        subprocess.call(['prodigal', '-i', fasta, '-a', protFileName], 
-                stdout=open(os.devnull, 'wb'), stderr=subprocess.STDOUT)
-    return protFileName
+        subprocess.call(['prodigal', '-i', fasta, '-a', prot_filename],
+                        stdout=open(os.devnull, 'wb'), stderr=subprocess.STDOUT)
+    return prot_filename
 
 def extract_gbk_trans(gbkfile, outfile=None):
     """Extract translated sequences from given GeneBank file and out to given
@@ -276,8 +276,8 @@ def extract_gbk_trans(gbkfile, outfile=None):
     if outfile:
         output_handle = open(outfile, mode='w+')
     else:
-        baseName = os.path.basename(gbkfile).split('.')[0]
-        outfile = baseName + "_translations.faa"
+        base_name = os.path.basename(gbkfile).split('.')[0]
+        outfile = base_name + "_translations.faa"
         output_handle = open(outfile, mode='w+')
     contig_n = 0
     cds_n = 0
@@ -293,7 +293,7 @@ def extract_gbk_trans(gbkfile, outfile=None):
                     header = ">" + feature.qualifiers['locus_tag'][0]
                 except KeyError:
                     continue
-                # some CDS do not have translations, retrieve nucleotide sequence 
+                # some CDS do not have translations, retrieve nucleotide sequence
                 # and translate
                 try:
                     assert feature.qualifiers['translation'][0]
@@ -313,9 +313,9 @@ def extract_gbk_trans(gbkfile, outfile=None):
                         continue
                     #conitnue # not sure why it was here. miscopied from above?
                     output_handle.write(header)
-                    output_handle.write(' # ' + start + ' # ' + end + ' # ' + 
-                            strand + ' # ID=' + str(contig_n) + '_' + str(cds_n)
-                            + ';')
+                    output_handle.write(' # ' + start + ' # ' + end + ' # ' +
+                                        strand + ' # ID=' + str(contig_n) + '_'
+                                        + str(cds_n) + ';')
                     output_handle.write(fasta_trans)
                     continue
                 # very occasionally translated seqs have two or more locations
@@ -325,21 +325,21 @@ def extract_gbk_trans(gbkfile, outfile=None):
                 if locs:
                     locs_list = locs.group(1).split(',')
                     for loc in locs_list:
-                        loc_str = ''.join( l for l in ''.join(loc) 
-                                if l not in "[]")
+                        loc_str = ''.join(l for l in ''.join(loc)
+                                          if l not in "[]")
                         loc_str = re.sub('\(|\)|:', ' # ', loc_str)
                         loc_str = re.sub('>|<', '', loc_str)
                         output_handle.write(" # " + loc_str)
                 else:
-                    loc_str = ''.join( l for l in ''.join(str(feature.location)) 
-                            if l not in "[]")
+                    loc_str = ''.join(l for l in ''.join(str(feature.location))
+                                      if l not in "[]")
                     loc_str = re.sub('\(|\)|\:', ' # ', loc_str)
                     loc_str = re.sub('>|<', '', loc_str)
                     output_handle.write(" # " + loc_str)
-                output_handle.write('ID=' + str(contig_n) + '_' + str(cds_n) 
-                        + ';')
+                output_handle.write('ID=' + str(contig_n) + '_' + str(cds_n)
+                                    + ';')
                 output_handle.write('\n' + feature.qualifiers['translation'][0]
-                        + '\n')
+                                    + '\n')
     input_handle.close()
     output_handle.flush()
     output_handle.close()
@@ -365,7 +365,7 @@ def main():
             sets of marker genes, as well as weighted versions of these statstics
             (including defining new weights for any given set).
             """,
-        epilog="""Report issues and bugs to the issue tracker at 
+        epilog="""Report issues and bugs to the issue tracker at
                 https://bitbucket.org/evolegiolab/micomplete or directly to 
                 eric@hugoson.org""")
 
@@ -428,31 +428,31 @@ def main():
                 raise RuntimeError('Unable to find hmmsearch in path')
 
     with open(args.sequence) as seq_file:
-        inputSeqs = [ seq.strip().split('\t') for seq in seq_file 
-                    if not re.match('#|\n', seq) ]
+        input_seqs = [seq.strip().split('\t') for seq in seq_file
+                      if not re.match('#|\n', seq)]
     
     if sys.version_info > (3, 0):
-        print(*inputSeqs, sep='\n', file=sys.stderr)
+        print(*input_seqs, sep='\n', file=sys.stderr)
     else:
-        for eachInput in inputSeqs: print(eachInput, file=sys.stderr)
+        for each_input in input_seqs: print(each_input, file=sys.stderr)
 
     ## print out column headers, unless linkage is requested
     if args.completeness and not args.linkage:
         if not args.hmms:
-            raise TypeError("""Completeness check requires a set of HMMs to 
+            raise TypeError("""Completeness check requires a set of HMMs to
             look for.""")
         if args.weights:
             print("Name\tLength\tGC-content\tPresent Markers\tCompleteness"
-                    "\tRedundance\tCompletenessW\tRedudanceW\tN50\tL50\tN90\tL90")
+                  "\tRedundance\tCompletenessW\tRedudanceW\tN50\tL50\tN90\tL90")
         else:
             print("Name\tLength\tGC-content\tPresent Markers\tCompleteness"
-                    "\tRedundance\tN50\tL50\tN90\tL90")
+                  "\tRedundance\tN50\tL50\tN90\tL90")
     elif not args.linkage:
         print("Name\tLength\tGC-content\tN50\tL50\tN90\tL90")
 
     if mp.cpu_count() < args.threads:
         raise RuntimeError('Specified number of threads are larger than the '
-                'number detected in the system: ' + mp.cpu_count())
+                           'number detected in the system: ' + mp.cpu_count())
     manager = mp.Manager()
     q = manager.Queue()
     pool = mp.Pool(processes=args.threads + 1)
@@ -463,9 +463,8 @@ def main():
     lq.setFormatter(logformatter)
     logger.addHandler(lq)
     logger.info("miComplete has started")
-    
     jobs = []
-    for i in inputSeqs: 
+    for i in input_seqs:
         if len(i) == 2:
             i.append(None)
         job = pool.apply_async(_worker, (i[0], i[1], args, q, i[2]))
@@ -480,7 +479,7 @@ def main():
     pool.join()
     if args.linkage:
         weights_output(weights_file)
-    logger.info("miComplete has finished")    
+    logger.info("miComplete has finished")
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
