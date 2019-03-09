cb3c8e14513190b034f4078a67b74e97142adc50
Eric Hugoson <eric@hugoson.org>
2019-03-09 14:29:57 +0100

    rc2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.rst', mode='r') as f:
     l_description = f.read()
 
 setup(name='micomplete',
-        version='1.0.0rc1',
+        version='1.0.0rc2',
         description='Quality control of assembled genomes',
         long_description=l_description,
         url='https://bitbucket.org/evolegiolab/micomplete',
@@ -13,7 +13,7 @@ setup(name='micomplete',
         license='GNU General Public License v3 or later (G  PLv3+)',
         
         classifiers=[
-            'Development Status :: 4 - Beta',
+            'Development Status :: 5 - Production/Stable',
             'Intended Audience :: Science/Research',
             'Topic :: Scientific/Engineering :: Bio-Informatics',
             'Environment :: Console',
