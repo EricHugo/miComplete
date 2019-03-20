a54e6427b940a56afd37039d9eaa5e607142ccf2
eric.hugoson <hugoson@evolbio.mpg.de>
2019-03-20 18:00:00 +0100

    added dunder version name to package

--- micomplete/__init__.py
+++ micomplete/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) Eric Hugoson.
 # See LICENSE for details.
 
+__version__ = '1.0rc2'
 __all__ = ['completeness', 'linkageanalysis', 'parseseqs', 'micomplete']
 from .completeness import calcCompleteness
 from .linkageanalysis import linkageAnalysis
