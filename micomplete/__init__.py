b31103ff8dcb7de5bcaa5dd3da8fc9f3fae050a3
eric.hugoson <hugoson@evolbio.mpg.de>
2019-07-10 16:25:47 +0200

    changed version to 1.1.0

--- micomplete/__init__.py
+++ micomplete/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) Eric Hugoson.
 # See LICENSE for details.
 
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 __all__ = ['completeness', 'linkageanalysis', 'parseseqs', 'micomplete']
 from .completeness import calcCompleteness
 from .linkageanalysis import linkageAnalysis
